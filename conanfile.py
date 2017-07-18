324be2691286034f4787fd083f65b6b4cf872862
Dario Ramos <dario.fenix@gmail.com>
2017-07-18 15:06:45 -0300

    Give full GCC version

--- conanfile.py
+++ conanfile.py
@@ -68,7 +68,8 @@ class ConanboostConan(ConanFile):
             flags.append("toolset=msvc-%s" % self._msvc_version())
         elif self.settings.compiler == "gcc":
             # For GCC we only need the major version otherwhise Boost doesn't find the compiler
-            flags.append("toolset=%s-%s"% (self.settings.compiler, self._gcc_short_version(self.settings.compiler.version)))
+            #flags.append("toolset=%s-%s"% (self.settings.compiler, self._gcc_short_version(self.settings.compiler.version)))
+            flags.append("toolset=%s-%s"% (self.settings.compiler, self.settings.compiler.version))
         elif str(self.settings.compiler) in ["clang"]:
             flags.append("toolset=%s-%s"% (self.settings.compiler, self.settings.compiler.version))
 
