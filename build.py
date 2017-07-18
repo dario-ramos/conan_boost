56d78f0beaca41f8566db737ba7e00697aea622e
Dario Ramos <dario.fenix@gmail.com>
2017-07-18 19:35:38 -0300

    Don't do debug builds

--- build.py
+++ build.py
@@ -4,4 +4,10 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="dario-ramos", channel="stable")
     builder.add_common_builds(shared_option_name="conan_boost:shared")
+    #builder.run()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release":
+             filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
